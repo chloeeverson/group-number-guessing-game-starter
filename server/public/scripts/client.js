fa77ee24b424b90cd3790878f1016bffbaa7c21b
Chloe Everson <chloeliz93@gmail.com>
2021-04-15 16:43:10 -0500

    added inputs and submit button for guesses

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,4 +2,21 @@ $(document).ready(handleReady);
 
 function handleReady() {
   console.log("jquery is loaded!")
+  randomNumber();
+  //on click of button do this event
+  ('#in-submit').on('click' , function(event) {
+    console.log('clicked');
+    //prevent refreshing of page
+    event.preventDefault();
+    //call add item function
+    submitGuess();
+  })
 }
+
+function randomNumber(min, max){
+  return Math.floor(Math.random() * (1 + max - min) + min);
+}
+
+randomNumber(1, 25);
+
+function submitGuess();
